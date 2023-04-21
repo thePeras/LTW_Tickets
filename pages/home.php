409a48cea9d56ad76033a6ccb262d8cead0fc25b
Francisco Ana <franciscopdana@gmail.com>
2023-04-21 12:51:03 +0100

    first steps on tickets main page

--- pages/home.php
+++ pages/home.php
@@ -11,6 +11,7 @@
     <link rel="stylesheet" href="css/layout.css">
     <link rel="stylesheet" href="css/theme.css">
     <link rel="stylesheet" href="css/remixicon.css">
+    <link rel="stylesheet" href="pages/tickets.css">
 </head>
 <body>
 
@@ -19,7 +20,24 @@
 ?>
 <main>
     <h1>Tickets</h1>
-    <p>Content to be added</p>
+    <ul id = "buttons">
+        <li><button type = "button"> Sort by</button></li>
+        <li><button type = "button"> New ticket</button></li>
+    </ul>
+    <ul id = "filters">
+        <li class = "active">Unassigned</li>
+        <li>Assigned to me</li>
+        <li>All tickets</li>
+        <li>Archived</li>
+    </ul>
+
+    <div class = "ticket-card">
+        <p>aqui está um ticket</p>
+    </div>
+
+    <div class = "ticket-card">
+        <p>aqui está outro ticket</p>
+    </div>
 </main>
     
 </body>
