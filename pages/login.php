c852ca879898a609653834e7af76b15aee1ef07d
thePeras <jtopereira@gmail.com>
2023-05-03 10:31:27 +0100

    Comoon components css refactor and ticket side bar actions pannel

--- pages/login.php
+++ pages/login.php
@@ -6,6 +6,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="css/layout.css" rel="stylesheet" type="text/css">
     <link href="css/login_register.css" rel="stylesheet" type="text/css">
+	<link href="css/components.css" rel="stylesheet" type="text/css">
 </head>
 <body>
     <div class="container">
--- pages/ticket.php
+++ pages/ticket.php
@@ -1,3 +1,58 @@
 <?php
+    require 'components/navbar/navbar.php';
+
     $id = $_GET['id'];
-    echo "Ticket ID: $id";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ticket #2</title>
+    <link rel="stylesheet" href="css/layout.css">
+    <link rel="stylesheet" href="css/theme.css">
+    <link rel="stylesheet" href="css/remixicon.css">
+    <link rel="stylesheet" href="css/components.css">
+    <link rel="stylesheet" href="css/ticket.css">
+</head>
+<body>
+    <?php
+        echo navbar();
+    ?>
+    <main class="ticket-page">
+        <div>
+            <h1>Ticket #2</h1>
+            <ul id = "buttons">
+                <li><button type = "button"> Status </button></li>
+                <li><button type = "button" class = "active"> Close </button></li>
+            </ul>
+        </div>
+        <div class="action-panel">
+            <div class="side-card">
+                <div>
+                    <h4 class="task-label">Assignee</h4>
+                    <p>
+                        <i class="ri-account-circle-line"></i>
+                        Unassigned
+                    </p>
+                </div>
+
+                <div>
+                    <h4 class="task-label">Team</h4>
+                    <p>
+                        <i class="ri-group-2-line"></i> 
+                        Unassigned
+                    </p>
+                </div>
+            </div>
+            <div class="side-card">
+                <h4 class="task-label">labels</h4>
+                <div>
+
+                </div>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
