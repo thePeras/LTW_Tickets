31ddf48d04b2593fdafbce83b0392ebee77a162f
thePeras <jtopereira@gmail.com>
2023-05-11 15:01:43 +0100

    Added a textarea to write a comment

--- pages/ticket.php
+++ pages/ticket.php
@@ -22,11 +22,25 @@
     ?>
     <main class="ticket-page">
         <div>
-            <h1>Ticket #2</h1>
+            <h1>Issue about log in #2</h1>
             <ul id = "buttons">
                 <li><button type = "button"> Status </button></li>
                 <li><button type = "button" class = "active"> Close </button></li>
             </ul>
+
+            <description>
+
+
+            </description>
+
+            <!-- Create a comment box -->
+            <div class="comment-box">
+                <h3>New comment</h3>
+                <textarea name="comment" id="comment" cols="30" rows="10" placeholder="Write your comment"></textarea>
+                <button type="button" class="primary">Submit</button>
+            </div>
+
+            <input type="text">
         </div>
         <div class="action-panel">
             <div class="side-card">
@@ -49,7 +63,10 @@
             <div class="side-card">
                 <h4 class="task-label">labels</h4>
                 <div>
-
+                    <p>
+                        <i class="ri-price-tag-3-line"></i>
+                        No labels assigned
+                    </p>
                 </div>
             </div>
         </div>
